43dae1be4e4ce0bb5771d1c2aaa96c29e7592221
UsmanZulfiqar <apple@apples-MacBook-Pro.local>
2023-12-01 12:26:45 +0500

    first commit

--- lib/core/helper/app_colors.dart
+++ lib/core/helper/app_colors.dart
@@ -1,54 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color transparent = Colors.transparent;
-  static const Color primaryLight = Color(0xFF292754);
-  static const Color primaryDark = Color(0xFFFFDF01);
-  static const Color lightBackgroundShade1 = Color.fromARGB(255, 191, 189, 235);
   static const Color lightBackgroundShade2 = Color.fromARGB(255, 226, 69, 69);
-
-  static const Color tertiary = Color(0xFFD81B76);
   static const Color green = Color.fromARGB(255, 92, 173, 107);
-  static const Color amber = Colors.amber;
   static const Color white = Color(0xFFFFFFFF);
-  static const Color whiteSmoke = Color(0xFFF4F4F4);
   static const Color black = Color(0xFF0D0D0D);
   static const Color grey = Colors.grey;
-  static const Color rangoonGreen = Color(0xFF171717);
-  static const Color borderColor = Color(0xFFDFDFDF);
-  static const Color dustyGrey = Color(0xFF9A9A9A);
-  static const Color carbonGrey = Color(0xFF5A5A5A);
-  static const Color lightGrey = Color(0xFFD6D6D6);
-  static const Color darkGrey = Color(0xFF383838);
-  static const Color nileBlue = Color(0xFF292754);
   static const Color red = Color(0xFFD2042D);
-  static const Color lightRed = Color(0xFFCF6679);
-  static const Color coralRed = Color(0xFFF64646);
-  static const Color lightPink = Color(0xFFFCD9D9);
-  static const Color steelGrey = Color(0xFF7C828A);
-
-  static const Color purple = Color(0xFF9875E2);
-  static const Color moodyBlue = Color(0xFF7873EC);
-  static const Color seaShell = Color(0xFFf2f0f6);
-  static const Color darkCyan = Color(0xFF0B8588);
-  static const Color midBlue = Color(0xFF2370AD);
-  static const Color catSkill = Color(0xFFF3F3F3);
-  static const Color greyCloud = Color(0xFF7B7B82);
-  static const Color softPeach = Color(0xFFEFEFEF);
-  static const Color lghtGrey = Color(0xFFf0f0f0);
-  static const Color lightRangoon = Color(0xFF242424);
-  static const Color addCallenderBG = Color(0xFFEDF4FF);
-  static const Color addScheduleBG = Color(0xFFFFEBF1);
-  static const Color popUpBg = Color(0xFF1F1F1F);
-  static const Color drawerExpansionTileBg = Color(0xFFF5F5F5);
-  static const Color testDateBG = Color(0xFFF5F6FF);
-  static const Color canceledChip = Color(0xFFF1DAD7);
-  static const Color completedChip = Color(0xFFDEF9E7);
-  static const Color completedChipText = Color(0xFF0EB014);
-  static const Color teal = Color(0xFF0E9A9D);
-  static const Color birch = Color(0xFF2E2C29);
-  static const Color recieverBG = Color(0xFFE6E6E6);
-
-
-  
 }
